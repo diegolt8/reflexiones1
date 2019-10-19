1fab3f59cf9527df993fda79c64aad152e050599
diegolt8 <diegoalugo@hotmail.com>
2019-10-18 23:25:25 -0500

    I upload reflections workshop

---
+++ src/claseDeclaracion/ClaseDeclaracion.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package claseDeclaracion;
+
+/**
+ *
+ * @author vi74ly
+ */
+public class ClaseDeclaracion {
+    
+    private int i;
+    
+    public void metodo(){
+        System.out.println("Hola");
+    }
+    
+}
